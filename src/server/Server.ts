1882f6d369809d05eff735afc09c0fafb705b228
MatheusStefaniDosSantos <matheusstefani8@gmail.com>
2023-10-02 04:13:55 -0300

    feat: Config inicial

---
+++ src/server/Server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+
+
+const server = express();
+
+server.get("/",(_,res)=>{
+    return res.send("'Galera, proximo semestre programa de verdade!' by Clerivaldo since 2022");
+});
+
+
+
+
+export{server};
