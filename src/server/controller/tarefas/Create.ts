2bbed887012f0bdcd748d3317d144f2f231c27c6
MatheusStefaniDosSantos <matheusstefani8@gmail.com>
2023-10-04 06:33:24 -0300

    feat: Começando a validar dados com yup

--- src/server/controller/tarefas/Create.ts
+++ src/server/controller/tarefas/Create.ts
@@ -1,16 +1,44 @@
 import { Request, Response } from "express";
 import { StatusCodes } from "http-status-codes";
+import * as yup from "yup";
 
-interface ITarefasProps {
+interface ITarefas {
     nome: string;
     link: string;
     dias: number;
 }
 
-export const create = (req: Request<{}, {}, ITarefasProps>, res: Response) => {
+const bodyValidation: yup.ObjectSchema<ITarefas> = yup.object().shape({
+    nome: yup.string().required().min(3),
+    link: yup.string().url().required(),
+    dias: yup.number().integer().positive().required(),
+});
+
+export const create = async (req: Request<{}, {}, ITarefas>, res: Response) => {
+    let validatedData: ITarefas | undefined = undefined;
+    try {
+        validatedData = await bodyValidation.validate(req.body);
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: {
+                default: yupError.message,
+            },
+        });
+    }
+
+    if (!validatedData) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: {
+                default: "Erro na validação dos dados!",
+            },
+        });
+    }
+
     return res.status(StatusCodes.CREATED).json({
-        nome: `O nome da tarefa foi cadastrado: ${req.body.nome}`,
-        link: `O link da sua tarefa foi cadastrado: ${req.body.link}`,
-        dias: `A quantidade de dias foi cadastrado: ${req.body.dias}`,
+        nome: `O nome da tarefa foi cadastrado: ${validatedData.nome}`,
+        link: `O link da sua tarefa foi cadastrado: ${validatedData.link}`,
+        dias: `A quantidade de dias foi cadastrado: ${validatedData.dias}`,
     });
 };
