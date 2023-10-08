9748e42e3eba1c453c87f983dcf362b66574ede5
MatheusStefaniDosSantos <matheusstefani8@gmail.com>
2023-10-08 15:53:38 -0300

    feat: Config inicial do knex

---
+++ src/server/database/knex/@types/knex.d.ts
@@ -0,0 +1,9 @@
+
+
+declare module "knex/types/tables" {
+    interface Tables {
+        //tarefaIngles: ITarefaIngles;
+        //tarefaLeetCode: ITarefaLeetCode;
+        //usuario: IUsuario
+    }
+}
